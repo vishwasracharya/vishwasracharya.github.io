6fbc1a7d5c68b1709062bf453efcaf0cf93d0890
Vishwas Acharya <vishwas.acharya@volansys.com>
2023-08-13 18:36:08 +0530

    Added scrollTo(0, 0) on navigation end to AppComponent.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+
+  private unsubscribe: Subscription[] = [];
+
+  constructor(
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    const routerSubscription = this.router.events.subscribe((event: any) => {
+      if (event instanceof NavigationEnd) {
+        window.scrollTo(0, 0);
+      }
+    })
+
+    this.unsubscribe.push(routerSubscription);
+  }
+
+  ngOnDestroy() {
+    this.unsubscribe.forEach((sb) => sb.unsubscribe());
+  }
 }
